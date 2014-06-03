d1222f15171d12c89717c7164a793a8f2ebc5bfa
Raffi Kudlac <raffi.kudlac@gmail.com>
2014-06-02 22:18:55 -0700

    header fixes

--- app/controllers/survey/tech/internet_tasks_controller.rb
+++ app/controllers/survey/tech/internet_tasks_controller.rb
@@ -10,7 +10,8 @@ end
 	private
 
 	def internettask_params
-		params.require(:internet_task).permit(:q1,:q2,:q3,:q4,:q5,:q6,:q7)
+		params.require(:internet_task).permit(:q1,:q2,:q3,:q4,:q5,:q6,:q7,:q8,:q9,:q10,:q11,:q12,:q13,:q14,:q15,
+			:q16,:q17)
 	end
 
 end
