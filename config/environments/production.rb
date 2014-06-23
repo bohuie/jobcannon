7a5e2d3f9be47efba8529b78f0ebe28a8c9a5098
Raffi Kudlac <raffi.kudlac@gmail.com>
2014-06-23 15:36:09 -0700

    shot in the dark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ First_Website::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.assets.css_compressor = :yui
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
