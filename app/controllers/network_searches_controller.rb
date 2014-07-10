6523d170d744a853abfb5c3950c22082543005aa
Matthew Bojey <mbojey@gmail.com>
2014-07-10 13:04:25 -0700

    Added Followers table
    Difference between two employee and employee employer friendship

--- app/controllers/network_searches_controller.rb
+++ app/controllers/network_searches_controller.rb
@@ -1,28 +1,19 @@
 class NetworkSearchesController < ApplicationController
 
   def create
+    puts "This is where I am"
     @search = NetworkSearch.new
     @search.update_attributes(search_params)
     @search.save
-    if @search.type == "name"
-      respond_to do |f|
-        f.js { render 'shared/ajax/name_search.js.erb' }
-      end
-    elsif @search.type == "name"
-      respond_to do |f|
-        f.js { render 'shared/ajax/skill_search.js.erb' }
-      end
-    else
-      respond_to do |f|
-        f.js { render 'shared/ajax/all_search.js.erb' }
-      end
+    respond_to do |f|
+      f.js { render 'shared/ajax/all_search.js.erb' }
     end
   end
   
   private
 
   def search_params
-        params.require(:network_search).permit(:user_id, :query, :type)
+        params.require(:network_search).permit(:user_id, :query, :qtype)
   end
 
 end
